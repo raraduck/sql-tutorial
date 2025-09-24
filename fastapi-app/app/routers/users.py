15f9d463c2b5362da4a7a9ea0478b434031c1bb1
dwnusa-win10 <dwnusa@gmail.com>
2025-09-24 16:00:32 +0900

    fastapi-app created

---
+++ fastapi-app/app/routers/users.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app import crud, schemas, database
+
+router =APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    return crud.cretae_user(db=db, user=user)
+
+@router.get("/", response_model=list[schemas.UserResponse])
+def read_users(db: Session = Depends(database.get_db)):
+    return crud.get_users(db)
