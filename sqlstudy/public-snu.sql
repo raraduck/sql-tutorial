b5a88f1b324720c3b15a7eb0fa77d161c84641f2
DAEUN KIM <raraducker@gmail.com>
2025-09-28 17:31:26 +0900

    update: classdb with sql

---
+++ sqlstudy/public-snu.sql
@@ -0,0 +1,83 @@
+create table department
+(dept_name varchar(20),
+building varchar(15),
+budget numeric(12,2) check (budget>0),
+primary key (dept_name));
+
+create table student
+(student_ID varchar(5),
+name varchar(20) not null,
+dept_name varchar(20),
+tot_cred numeric(3,0) check (tot_cred>=0),
+primary key (student_ID),
+foreign key (dept_name) references department(dept_name) on delete set null);
+
+create table course
+(course_id varchar(7),
+title varchar(50),
+dept_name varchar(20),
+credits numeric(2,0) check (credits>0),
+primary key (course_id),
+foreign key (dept_name) references department(dept_name) on delete set null);
+
+create table instructor(
+	instructor_ID varchar(5),
+	name varchar(20) not null,
+	dept_name varchar(20),
+	salary numeric(8,2) check (salary>29000),
+	primary key (instructor_ID),
+	foreign key (dept_name) references department (dept_name) on delete set null
+);
+
+create table all_list(
+	course_id 	varchar(7),
+	seq_id 		varchar(8),
+	semester 	varchar(6) check (semester in ('Fall','Winter','Spring','Summer')), 
+	year 		numeric(4,0) check (year>1701 and year<2100),
+	building 	varchar(15),
+	room_number varchar(7),
+	class_time 	varchar(1),
+	primary key (course_id, seq_id, semester, year),
+	foreign key (course_id) references course (course_id) on delete cascade
+);
+
+
+-- CREATE TABLE all_list (
+--     course_id   varchar(7),
+--     seq_id      varchar(8),
+--     semester    varchar(6) check (semester in ('Fall','Winter','Spring','Summer')), 
+--     year        integer check (year > 1701 and year < 2100),
+--     building    varchar(15),
+--     room_number varchar(7),
+--     class_time  varchar(1),
+--     primary key (course_id, seq_id, semester, year),
+--     foreign key (course_id) references course (course_id) on delete cascade
+-- );
+
+create table take_course
+(student_ID varchar(5),
+course_id varchar(8),
+seq_id varchar(8),
+semester varchar(6),
+year numeric(4,0),
+grade varchar(2),
+primary key (student_ID, course_id, seq_id, semester, year),
+foreign key (course_id, seq_id, semester, year) references all_list (course_id, seq_id, semester, year) on delete cascade,
+foreign key (student_ID) references student (student_ID) on delete cascade);
+
+-- LOAD DATA  INFILE 'department.txt' INTO TABLE department;
+
+COPY department FROM 'C:/ClassDB_0605/department.txt' DELIMITER E'\t' NULL '' CSV;
+COPY all_list FROM 'C:/ClassDB_0605/all_list.txt' DELIMITER E'\t' NULL '';
+COPY course FROM 'C:/ClassDB_0605/course.txt' DELIMITER E'\t' NULL '';
+COPY instructor FROM 'C:/ClassDB_0605/instructor.txt' DELIMITER E'\t' NULL '';
+COPY student FROM 'C:/ClassDB_0605/student.txt' DELIMITER E'\t' NULL '';
+COPY take_course FROM 'C:/ClassDB_0605/take_course.txt' DELIMITER E'\t' NULL '';
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS all_list CASCADE;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS instructor;
+DROP TABLE IF EXISTS student CASCADE;
+DROP TABLE IF EXISTS take_course CASCADE;
+
